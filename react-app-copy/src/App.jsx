f77e8395c90afaa399b1fec5728544ff441960f2
areya209 <areya0133@gmail.com>
2021-10-20 22:44:06 +0200

    added my to do list

--- react-app-copy/src/App.jsx
+++ react-app-copy/src/App.jsx
@@ -1,3 +1,21 @@
+import { List } from "./components/Todolist";
+
 export const App = () => {
-  return <h1>App</h1>;
+  const lists = [
+    { title: "My First List", body: "Wake Up Early" },
+    { title: "My Second List", body: "Have my Breakfast" },
+    { title: "My Third List", body: "Attend my lectures in ITI" },
+    { title: "My Fourth List", body: "Doing My Labs and Tasks" },
+    { title: "My Fifth List", body: "Take a Break and having my Lunch" },
+    { title: "My Sixth List", body: "Develop myself in Web Development" },
+  ];
+  return (
+    <div>
+      <h2>Welcome to my own ToDoList</h2>
+      {lists.map((list) => (
+        <List title={list.title} body={list.body} />
+      ))}
+
+    </div>
+  );
 };
---
+++ react-app-copy/src/components/Todolist.jsx
@@ -0,0 +1,9 @@
+export const List = (props) => {
+    return (
+        <div>
+            <h4>{props.title}</h4>
+            <p>{props.body}</p>
+            
+        </div>
+    );
+};
